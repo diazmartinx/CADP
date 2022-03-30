4c8fcefe9a05f604d94cd52b3a75e01c90662c65
Martin Diaz <69984284+diazmartini@users.noreply.github.com>
2022-03-29 22:31:52 -0300

    ej2a

---
+++ TP 1 parte 2/ej2a.pas
@@ -0,0 +1,36 @@
+program ej2;
+
+{
+2. Realice un programa que lea 10 números e informe cuál fue el mayor número leído. Por ejemplo, si
+se lee la secuencia:
+3 5 6 2 3 10 98 8 -12 9
+deberá informar: “El mayor número leído fue el 98”
+
+a. Modifique el programa anterior para que, además de informar el mayor número leído, se
+informe el número de orden, dentro de la secuencia, en el que fue leído. Por ejemplo, si se lee la
+misma secuencia:
+3 5 6 2 3 10 98 8 -12 9
+deberá informar: “El mayor número leído fue el 98, en la posición 7”
+}
+
+var
+    i, pos    : byte;
+    num, max  : integer;
+
+begin
+    for i:=1 to 10 do begin
+        writeln('Ingrese numero ',i,':');
+        readln(num);
+        if i = 1 then begin // primera iteracion
+            max := num;
+            pos := i;
+        end
+        else
+            if num>max then begin
+                max := num;
+                pos := i;
+            end
+    end;
+    writeln('El mayor numero leido fue el ',max,', en la posicion ',pos);
+    read();
+end.
