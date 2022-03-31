472800f59fa0681d547635c2ea5e9c841075ac80
Martin Diaz <69984284+diazmartini@users.noreply.github.com>
2022-03-30 23:53:24 -0300

    Update ej4a.pas

--- TP 1 parte 2/ej4a.pas
+++ TP 1 parte 2/ej4a.pas
@@ -6,40 +6,30 @@ a. Modifique el ejercicio anterior para que, en vez de leer 1000 números, la le
 el número 0, el cual debe procesarse.
 b. Modifique el ejercicio anterior para que, en vez de leer 1000 números, la lectura finalice al leer
 el número 0, el cual no debe procesarse
-
 }
 
 var
     num, min1, min2 :integer;
-    cont :word;
+    cont:word;
     
 begin
     cont := 0;
+    min1 := 32767;
+    min2 := 32767;
+    
     repeat
         cont := cont + 1;
         
         writeln('Ingrese numero ',cont,' (0 para finalizar)');
         readln(num);
         
-        if cont = 1 then // inicializa min1 
-            min1 := num
-        else
-            if cont = 2 then  // inicializa min2
-                if num<min1 then begin // mantiene el mas chico en min1
-                    min2 := min1;
-                    min1 := num;
-                    
-                end
-                else
-                    min2 := num
+        if num < min1 then begin // mantiene el mas chico en min1
+            min2 := min1;
+            min1 := num;
+        end
             else
-                if num < min1 then begin
-                    min2 := min1;
-                    min1 := num;
-                end
-                else
-                    if num < min2 then
-                        min2 := num;
+                if num < min2 then
+                    min2 := num;
         
     until ( (num = 0) or (cont = 1000) );
     
